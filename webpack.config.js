a7ed5414c1887ac7491ae7d003e34dfcef8cc83a
Oleg Borodin <onborodin@gmail.com>
2018-10-12 14:03:57 +0200

    work commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,34 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
-  entry: "./src/react-app.js",
-  output: {
-    path: path.join(__dirname, "/build"),
-    filename: "react-app.js"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/react-app.html",
-      filename: "react-app.html",
-      minify: true
+    entry: "./src/react-app.js",
+    output: {
+        path: path.join(__dirname, "/build"),
+        filename: "react-app.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/react-app.html",
+            filename: "react-app.html",
+            minify: true
     }),
     new CopyWebpackPlugin([
             { from: './src/static' }
     ])
   ],
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
   }
 };
